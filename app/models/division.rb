3a13d57d608db4aff0b1358063cec430c5b5e30c
Paul McCarthy <paul.mccarthy@avant.com>
2017-12-03 23:08:04 -0600

    modelling for standings

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,3 +1,8 @@
 class Division < ApplicationRecord
   has_many :teams
+
+  def standings
+    teams = self.teams.order(wins: :desc)
+    return teams.map {|team| {position: teams.index(team), team: team.standing_friendly_team}}
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,17 @@
 class Team < ApplicationRecord
   belongs_to :division
   has_one :stadium
+
+  def winning_pct
+    wins.to_f/(wins.to_f + losses.to_f).round(3)
+  end
+
+  def standing_friendly_team
+    {
+      team: self.abbrev,
+      wins: self.wins,
+      losses: self.losses,
+      winning_pct: self.winning_pct
+    }
+  end
 end
